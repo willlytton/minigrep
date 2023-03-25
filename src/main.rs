5b2b6832ade119804840216dd1a8e5eece17fcd6
willmaple <liam.maple00@gmail.com>
2023-03-25 12:06:42 -0400

    added arg and parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-// allows you to pass arguments to the commands line
-use std::env::args;
+use std::env; // allows you to pass arguments to the commands line
+use std::fs; // allows for file system manipulation
 
 fn main() {
-   let args: Vec<String> = env::args().collect();
-   dbg!(args);  // dbg! returns reference value 
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);  // dbg! returns reference value
+
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+    .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+//
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = &args[1].clone();
+    let file_path = &args[2].clone();
+
+    Config { query, file_path }
 }
